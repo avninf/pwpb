748c48850c08018030f519a9ca7e374b319d9c42
avninf <93563175+avninf@users.noreply.github.com>
2022-03-18 13:20:20 +0700

    Add files via upload

---
+++ ujikom_avni/admin/views/user/Index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avni</title>
+    <style>
+      body {
+  height: 3300px;
+}
+
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <!-- My CSS -->
+    <link rel="stylesheet" href="../public/asset/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow" style="background-color: grey;">
+        <div class="container">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#About">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#Project">Project</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#contact">Contact</a>
+              </li>
+               <li class="nav-item">
+                <a class="nav-link" href ="./admin"><i class="bi bi-gear"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!-- Akhir -->
+    <!-- Jumboton -->
+    
+      <div class="jumbotron text-center mt-0" style="height: 500px">
+          <img src="../public/asset/img/a.jpg" alt="" width="200" class="rounded-circle" data-aos="zoom-in=up" data-aos-duration="2000" />
+        <h1 class="display-4">AvniNF</h1>
+       <p class="lead">Siswa | SMKN4 TASIKMALAYA</p>
+       </div>
+       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#a6a4a4" fill-opacity="1" d="M0,160L30,154.7C60,149,120,139,180,117.3C240,96,300,64,360,53.3C420,43,480,53,540,96C600,139,660,213,720,245.3C780,277,840,267,900,229.3C960,192,1020,128,1080,101.3C1140,75,1200,85,1260,74.7C1320,64,1380,32,1410,16L1440,0L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"></path></svg> 
+          <!-- Akhir about -->
+         
+      <!-- Akhir -->
+      <!-- About -->
+    <section id="About" style="height: 200px;">
+      <div class="row text-center" data-aos="flip-right"data-aos-duration="1000">
+        <h2>About Me</h2>
+      </div>
+      <div class="row justify-content-center fs=5">
+        <div class="col-sm-4"
+      </div>
+      <div class="row justify-content-center fs-5">
+        <div class="col-sm-4" data-aos="fade-up"data-aos-duration="3000">
+        <p>27 april 2004 hari selasa</p>
+      </div>
+      <div class="col-sm-4"data-aos="fade-down"data-aos-duration="3000">
+        <p>Hobi rebahan sambil baca wattpad</p>
+      </div>
+    </div>
+    </section>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#a6a4a4" fill-opacity="1" d="M0,224L40,208C80,192,160,160,240,160C320,160,400,192,480,181.3C560,171,640,117,720,122.7C800,128,880,192,960,186.7C1040,181,1120,107,1200,96C1280,85,1360,139,1400,165.3L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+       
+   <!-- Akhir about -->
+    <!-- Project-->
+    <section id="project">
+        <div class="container">
+        <div class="row text-center" data-aos="fade-right" data-aos-duration="2000">
+           <h2>project</h2>
+           </div>
+           <div class="row justify-content-evenly fs-5">
+              <div class="col-sm-4">
+                <div class="card" data-aos="flip-left" data-aos-duration="2000" style="width:18rem;">
+                    <img src="../public/asset/img/a.jpg"
+                        class="card-img-top" alt="..."/>
+                        <div class="card body">
+                        <p class="card-text">Avni
+                        </p>
+                        </div>
+                </div>
+              </div>
+              </section>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#a6a4a4" fill-opacity="1" d="M0,224L40,208C80,192,160,160,240,160C320,160,400,192,480,181.3C560,171,640,117,720,122.7C800,128,880,192,960,186.7C1040,181,1120,107,1200,96C1280,85,1360,139,1400,165.3L1440,192L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path></svg>
+    
+       
+      
+    <!--Akhir project-->
+    <!--contac-->
+  <section>
+    <div class="container pt-5"></div>
+      <div class="row text-center mb-5">
+        <h2 >Contact</h2>
+      </div>
+      <div class="row justify-content-center" data-aos="fade-right" data-aos duration="2000">
+      <div class="col-6">
+      <form method="POST">
+        <div class="mb-3">
+          <label for="Email" class="form-label">Email address</label>
+          <input type="email" name="Email" class="form-control"id="Email" aria-describedby="emailHelp" required="">
+          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+          <label for="Nama" class="form-label">Nama</label>
+          <input type="text" name="Nama_k" class="form-control" id="nama"required="">
+        </div>
+        <label for="floatingTextarea">Pesan</label>
+        <div class="form-floating">
+          <textarea class="form-control" placeholder="Leave a comment here" name="Pesan" id="floatingTextarea" required=""></textarea>
+        </div>
+        <button type="submit" name="submit" class="btn btn-primary mt-3">Kirim</button>
+      </form>
+      </div>
+      </div>
+  </section>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000000" fill-opacity="1" d="M0,224L60,192C120,160,240,96,360,112C480,128,600,224,720,224C840,224,960,128,1080,122.7C1200,117,1320,203,1380,245.3L1440,288L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+      <footer class="text-center text-white  pb-5" style="background-color: #000000;">
+        <p>Create with <i class="bi bi-heart-fill"></i> by <a href="">Avnif27</a></p>
+      </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+    </body>
+    </html>
